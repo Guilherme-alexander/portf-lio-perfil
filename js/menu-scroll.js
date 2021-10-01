3e4304f93c5393f6282145a982fcff59df5d2c61
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2021-09-30 21:03:34 -0300

    atualização arquivos

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -1,12 +1,45 @@
+//JQUERY
 $(function(){
 
-    $('nav a').click(function(){
-        var href = $(this).attr('href');
-        var offSetTop = $(href).offset().top;
+//effect menu scroll top
+$('nav a').click(function(){
+    var href = $(this).attr('href');
+    var offSetTop = $(href).offset().top;
 
-        $('html,body').animate({'scrollTop':offSetTop,})
+    $('html,body').animate({'scrollTop':offSetTop,})
 
-        return false;
-    })
+    return false;
+})
+
+//menu Responsivo mobile
+$('.fa-bars').click(function(){
+    $('.menu-mobile').find('ul').slideToggle();
+})
+
+
+//menu filtro portifólio
+$(document).on('click','.menu-portfolio p',function(){
+    $(this).addClass('selected').siblings().removeClass('selected')
+    // console.log(this)
+    // console.log(this.id)
+    if(this.id == 'frontEnd'){
+        $('.box-works-single [value=frontEnd]').css('display','block')
+        $('.box-works-single [value=designer]').css('display','none')
+    }
+    if(this.id == 'designer'){
+        $('.box-works-single [value=designer]').css('display','block')
+        $('.box-works-single [value=frontEnd]').css('display','none')
+    }
+    if(this.id == 'aii'){
+        $('.box-works-single [value=designer]').css('display','block')
+        $('.box-works-single [value=frontEnd]').css('display','block')
+        
+    }
+    if(this.id == 'code'){
+        // console.log('ok')
+    }    
+})
+
+
 
 })
--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// JAVASCRIPT
 
+// Slider works
+var lastIndex = 0;
+
+    var imagem = document.querySelectorAll('.box-work')
+    var bullets = document.querySelectorAll('.bullets-single')
+
+    imagem.forEach((item,index)=>{
+        bullets[index].addEventListener('click',()=>{
+            let lastImage = imagem[lastIndex]
+            let actualImage = imagem[index]
+
+            bullets[lastIndex].classList.remove('active-bullets')
+            bullets[index].classList.add('active-bullets')
+
+            lastImage.style.opacity = 0;
+            actualImage.style.opacity = 1;
+            lastIndex = index;
+        })
+      
+});
