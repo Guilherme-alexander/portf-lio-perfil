e8aa6cba5f819cf07b3a1a11b1b59b922909da7a
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2021-11-09 17:57:03 -0300

    conserto de alguns bugs

--- js/script.js
+++ js/script.js
@@ -5,30 +5,37 @@ const menuMobile = document.querySelector('.menu-mobile ul');
 const btnMobile = document.querySelector('.fa-bars');
 
 btnMobile.addEventListener('click',function(){ 
-    if(menuMobile.classList.contains('active')){
-        menuMobile.classList.remove('active');
-    }else{
-        menuMobile.classList.add('active');
-    }
+
+        if(menuMobile.classList.contains('active')){
+            menuMobile.classList.remove('active')
+        }else{
+            menuMobile.classList.add('active');
+        }
+    
+    
 })
+
 
 // Slider works
 var lastIndex = 0;
 
-    var imagem = document.querySelectorAll('.box-work')
-    var bullets = document.querySelectorAll('.bullets-single')
+var imagem = document.querySelectorAll('.box-work')
+var links = document.querySelectorAll('.box-work [href]')
+var bullets = document.querySelectorAll('.bullets-single')
 
     imagem.forEach((item,index)=>{
-        bullets[index].addEventListener('click',()=>{
-            let lastImage = imagem[lastIndex]
-            let actualImage = imagem[index]
+    bullets[index].addEventListener('click',()=>{
 
+            var lastImage = imagem[lastIndex]
+            var actualImage = imagem[index]
+    
             bullets[lastIndex].classList.remove('active-bullets')
-            bullets[index].classList.add('active-bullets')
-
+            bullets[index].classList.add('active-bullets')  
+            
             lastImage.classList.remove('opacity')
             actualImage.classList.add('opacity')
-            lastIndex = index;
+            links[index].href
+            lastIndex = index;     
             
         })
 });
